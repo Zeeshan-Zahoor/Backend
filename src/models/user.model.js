e3bbcf6d994ccf72ca61db8ce8d6850ab58b6f51
Zeeshan-Zahoor <shanmohd3939@gmail.com>
2026-03-13 19:32:17 +0530

    Using Postman for backend

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -48,12 +48,15 @@ const userSchema = new Schema({
 }, {timestamps: true})
 
 
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
-        return next();
+userSchema.pre("save", async function() {
+    try {
+        if (!this.isModified("password")) {
+            return next();
+        }
+        this.password = await bcrypt.hash(this.password, 10);
+    } catch (error) {
+        throw error;
     }
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
 })
 
 userSchema.methods.isPasswordCorrect = async function (password) {
